ec95a32ff4d9e18770f48c28f537ce1176a1dbc6
Emmanuel Narcisse <73095609+Emmanuel97423@users.noreply.github.com>
2021-03-17 11:56:04 +0400

    Update like.js

--- routes/liked.js
+++ routes/liked.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
 const likeCtrl = require("../controllers/liked");
 
-router.post("/:id/like", auth, likeCtrl.like);
+router.post("/:id/like",  likeCtrl.like);
 
 module.exports = router;
